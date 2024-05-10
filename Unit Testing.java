57e3d0f2953c326c803db4421f45630fe9f4b614
bhargavi1021 <169147735+bhargavi1021@users.noreply.github.com>
2024-05-10 14:39:35 +0530

    Update Unit Testing.java

--- Unit Testing.java
+++ Unit Testing.java
@@ -1,14 +1,57 @@
+
 public class Main {
-    public static void toUppercase(char[] str) {
-        for (int i = 0; i < str.length; ++i) {
-            if (Character.isLetter(str[i]) && Character.isLowerCase(str[i])) {
-                str[i] = Character.toUpperCase(str[i]);
-            }
+    public static String toUpper(String str){
+    String s = "";
+    for(int i=0 ; i<str.length();i++){
+        if(Character.isLowerCase(str.charAt(i))){
+            s += (char)(str.charAt(i)-32);
         }
-        System.out.println(str);
+        else{
+            s += str.charAt(i)+"";
+        }
     }
-    public static void main(String[] args) {
-        char[] str = "hello world".toCharArray();
-        toUppercase(str);
+    return s;
+}
+    public static boolean test_case1(String str){
+        String compare="HELLO WORLD";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case2(String str){
+        String compare="HELLO WORLD";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case3(String str){
+        String compare="HELLO3WORLD";
+        return toUpper(str).equals(compare);
+    }
+     public static boolean test_case4(String str){
+        String compare="HELLOW32ORLD";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case5(String str){
+        String compare="@#$%";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case6(String str){
+        String compare="5664136";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case7(String str){
+        String compare="HAI@BYE";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case8(String str){
+        String compare="HELLO$%#`	`";
+        return toUpper(str).equals(compare);
+    }
+public static void main(String[] args) {
+        System.out.println(test_case1("hello world"));
+        System.out.println(test_case2("HELLO WORLD"));
+        System.out.println(test_case3("hello3world"));
+        System.out.println(test_case4("HELLOW32ORLD"));
+        System.out.println(test_case5("@#$%"));
+        System.out.println(test_case6("5664136"));
+        System.out.println(test_case7("hai@bye"));
+        System.out.println(test_case8("HELLO$%#"));
     }
 }
